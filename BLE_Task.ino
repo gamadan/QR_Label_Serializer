ad8bef10cd5b8367fb00d153982c8de44b3f61b9
gamadan <108008945+gamadan@users.noreply.github.com>
2023-09-19 15:10:35 -0500

    Add files via upload

--- BLE_Task.ino
+++ BLE_Task.ino
@@ -1,9 +1,14 @@
-#define SERVICE_UUID                    "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define UNIT_CHARACTERISTIC_UUID        "beb54830-36e1-4688-b7f5-ea07361b26a8"
-#define CHANNEL_CHARACTERISTIC_UUID     "beb54831-36e1-4688-b7f5-ea07361b26a8"
-#define ADDUNIT_CHARACTERISTIC_UUID     "beb54842-36e1-4688-b7f5-ea07361b26a8"
-#define REMOVEUNIT_CHARACTERISTIC_UUID  "beb54843-36e1-4688-b7f5-ea07361b26a8"
-#define RAWDATA_CHARACTERISTIC_UUID  "beb54844-36e1-4688-b7f5-ea07361b26a8"
+#define PRINT_LABEL_SERVICE_UUID        "2e01ae6e-5726-11ee-8c99-0242ac120002"
+#define ADD_REMOVE_SERVICE_UUID         "2e01b27e-5726-11ee-8c99-0242ac120002"
+#define RAWDATA_SERVICE_UUID            "2e01b3dc-5726-11ee-8c99-0242ac120002"
+#define UNIT_CHARACTERISTIC_UUID        "2e01b4f4-5726-11ee-8c99-0242ac120002"
+#define CHANNEL_CHARACTERISTIC_UUID     "2e01b986-5726-11ee-8c99-0242ac120002"
+#define ADDUNIT_CHARACTERISTIC_UUID     "2e01baa8-5726-11ee-8c99-0242ac120002"
+#define REMOVEUNIT_CHARACTERISTIC_UUID  "2e01bbac-5726-11ee-8c99-0242ac120002"
+#define RAWDATA_CHARACTERISTIC_UUID     "2e01bcb0-5726-11ee-8c99-0242ac120002"
+
+
+
 
 TickType_t lastReceivedTick = xTaskGetTickCount();
 bool bDeviceConnected, bOldDeviceConnected = false;
@@ -16,77 +21,142 @@ std::string rxChannelMessage = "";
 
  */
 
-void ble_task(void * parameters) {  
-  
-  BLEDevice::init("QR Serializer");
-  BLEServer *pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new ServerCallbacks(&bDeviceConnected));
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  
-  unitCharacteristic = pService->createCharacteristic(
+void initUnitCharacteristic() {
+  unitCharacteristic = printLabelService->createCharacteristic(
                                          UNIT_CHARACTERISTIC_UUID,
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
-  channelCharacteristic = pService->createCharacteristic(
+                                       
+  unitCharacteristic->setCallbacks(new Unit_CharCallbacks(&lastReceivedTick, &rxUnitMessage));
+
+  
+  
+}
+
+
+void initChannelCharacteristic() {
+  channelCharacteristic = printLabelService->createCharacteristic(
                                          CHANNEL_CHARACTERISTIC_UUID,
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
-  addUnitCharacteristic = pService->createCharacteristic(
+                                       
+  channelCharacteristic->setCallbacks(new Channel_CharCallbacks(&lastReceivedTick, &rxChannelMessage));
+
+}
+
+void initAddUnitCharacteristic() {
+  addUnitCharacteristic = addRemoveService->createCharacteristic(
                                          ADDUNIT_CHARACTERISTIC_UUID,
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
-  removeUnitCharacteristic = pService->createCharacteristic(
+
+  addUnitCharacteristic->setCallbacks(new AddUnit_CharCallbacks(&rxAddUnitMessage));
+
+
+}
+
+void initRemoveUnitCharacteristic() {
+  removeUnitCharacteristic = addRemoveService->createCharacteristic(
                                          REMOVEUNIT_CHARACTERISTIC_UUID,
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
 
-  rawDataCharacteristic = pService->createCharacteristic(
+  removeUnitCharacteristic->setCallbacks(new RemoveUnit_CharCallbacks(&rxRemoveUnitMessage));
+
+}
+
+void initRawDataCharacteristic() {
+  rawDataCharacteristic = rawDataService->createCharacteristic(
                                          RAWDATA_CHARACTERISTIC_UUID,
                                          BLECharacteristic::PROPERTY_WRITE
                                        );
 
+  rawDataCharacteristic->setCallbacks(new RawData_CharCallbacks(&uart));
 
-  unitCharacteristic->setCallbacks(new Unit_CharCallbacks(&lastReceivedTick, &rxUnitMessage));
-  channelCharacteristic->setCallbacks(new Channel_CharCallbacks(&lastReceivedTick, &rxChannelMessage));
-  addUnitCharacteristic->setCallbacks(new AddUnit_CharCallbacks(&rxAddUnitMessage));
-  removeUnitCharacteristic->setCallbacks(new RemoveUnit_CharCallbacks(&rxRemoveUnitMessage));
-  removeUnitCharacteristic->setCallbacks(new RawData_CharCallbacks(&uart));
+  
+}
 
-  rawDataCharacteristic->addDescriptor(new BLE2902());
+void ble_task(void * parameters) {  
+  
+  BLEDevice::init("QR Serializer");
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new QR_Label_Serializer_ServerCallbacks(&bDeviceConnected));
+  printLabelService = pServer->createService(PRINT_LABEL_SERVICE_UUID);
+  addRemoveService = pServer->createService(ADD_REMOVE_SERVICE_UUID);
+  rawDataService = pServer->createService(RAWDATA_CHARACTERISTIC_UUID);
+
+
+
+  initUnitCharacteristic();
+  initChannelCharacteristic();
+  initAddUnitCharacteristic();
+  initRemoveUnitCharacteristic();
+  initRawDataCharacteristic();
+
+  
+/*
+ * For some reason, this doesn't work correctly. 
+ * 
+ * 
+  BLEDescriptor unit_Descriptor("2901"); // Create descriptor
+  BLEDescriptor channel_Descriptor("2901"); // Create descriptor
+  BLEDescriptor addUnit_Descriptor("2901"); // Create descriptor
+  BLEDescriptor removeUnit_Descriptor("2901"); // Create descriptor
   BLEDescriptor rawDataDescriptor("2901"); // Create descriptor
-  rawDataDescriptor.setValue("Send Raw Print Data"); // Set descriptor value
-  unitCharacteristic->addDescriptor(&rawDataDescriptor); // Add descriptor to characteristic
+  
 
   unitCharacteristic->addDescriptor(new BLE2902());
-  BLEDescriptor unit_Descriptor("2901"); // Create descriptor
   unit_Descriptor.setValue("Print Unit"); // Set descriptor value
   unitCharacteristic->addDescriptor(&unit_Descriptor); // Add descriptor to characteristic
 
   channelCharacteristic->addDescriptor(new BLE2902());
-  BLEDescriptor channel_Descriptor("2901"); // Create descriptor
   channel_Descriptor.setValue("Print Channel"); // Set descriptor value
-  channelCharacteristic->addDescriptor(&channel_Descriptor); // Add descriptor to characteristic
 
-  
-
-  addUnitCharacteristic->addDescriptor(new BLE2902());
-  BLEDescriptor addUnit_Descriptor("2901"); // Create descriptor
+  addUnitCharacteristic->addDescriptor(new BLE2902());  
   addUnit_Descriptor.setValue("Add unit"); // Set descriptor value
   addUnitCharacteristic->addDescriptor(&addUnit_Descriptor); // Add descriptor to characteristic
-
-  removeUnitCharacteristic->addDescriptor(new BLE2902());
-  BLEDescriptor removeUnit_Descriptor("2901"); // Create descriptor
+  
+  removeUnitCharacteristic->addDescriptor(new BLE2902());  
   removeUnit_Descriptor.setValue("Remove unit"); // Set descriptor value
   removeUnitCharacteristic->addDescriptor(&removeUnit_Descriptor); // Add descriptor to characteristic
 
+  rawDataCharacteristic->addDescriptor(new BLE2902());  
+  rawDataDescriptor.setValue("Raw data"); // Set descriptor value
+  unitCharacteristic->addDescriptor(&rawDataDescriptor); // Add descriptor to characteristic
 
+  Serial.println("unit_Descriptor discriptor:");
+  Serial.println(unit_Descriptor.toString().c_str());
+  Serial.println("");
+
+  Serial.println("asdfasdf discriptor:");
+  Serial.println(channel_Descriptor.toString().c_str());
+  Serial.println("");
+
+  Serial.println("addUnit_Descriptor discriptor:");
+  Serial.println(addUnit_Descriptor.toString().c_str());
+  Serial.println("");
+
+  Serial.println("removeUnit_Descriptor discriptor:");
+  Serial.println(removeUnit_Descriptor.toString().c_str());
+  Serial.println("");
+
+  Serial.println("rawDataDescriptor discriptor:");
+  Serial.println(rawDataDescriptor.toString().c_str());
+  Serial.println("");
+*/
+
+  
 
   delay(500);
   
-  pService->start();
+  printLabelService->start();
+  addRemoveService->start();
+  rawDataService->start();
   
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->addServiceUUID(PRINT_LABEL_SERVICE_UUID);
+  pAdvertising->addServiceUUID(ADD_REMOVE_SERVICE_UUID);
+  pAdvertising->addServiceUUID(RAWDATA_SERVICE_UUID);
   pAdvertising->setScanResponse(true);
   pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
   pAdvertising->setMinPreferred(0x12);
